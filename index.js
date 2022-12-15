664fe18bbb6dfdd2de165ec2393bc68108e96dde
Alken <e1595771@cmaisonneuve.qc.ca>
2022-12-15 11:26:45 -0500

    ajout function

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+let nb = 0;
 
 function addition() {
-    
+    nb += 1;
+    document.getElementById("number").innerText = nb;
+}
+
+function sustraction() {
+    nb -= 1;
+    document.getElementById("number").innerText = nb;
 }
